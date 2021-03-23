4945228a039df1656058de79bc4b45449b3c3240
cwavedave <david@creativewavelength.co.uk>
2021-03-23 14:51:29 +0100

    Finished

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+#MITx
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def fib_efficient(n,d):
+    if n in d:
+        return (d[n])
+    else:
+        ans = fib_efficient(n-1,d) + fib_efficient(n-2,d)
+        d[n] = ans
+        return ans
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+d = {1:1, 2:2}
+print(fib_efficient(4,d))
